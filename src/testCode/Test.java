00ed234840367182e949451f46da60049d4a6bd8
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-01-15 10:24:39 +0100

    local commit test end

--- src/testCode/Test.java
+++ src/testCode/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
 		System.out.println("Test 2");
-		
+		System.out.println("local commit test end");
 
 	}
 
