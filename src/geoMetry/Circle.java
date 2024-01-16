96b38c4d413f935b780c4879eaac2d64b644c100
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-01-16 09:44:39 +0100

    newest written part of class "circle"

--- src/geoMetry/Circle.java
+++ src/geoMetry/Circle.java
@@ -1,5 +1,26 @@
 package geoMetry;
 
-public class Circle {
+public class Circle extends Product{
+	
+	public double radius;
+
+	public Circle(double x, double y, double radius) {
+		super();
+		this.radius = radius;
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	@Override
+	public double calculateCircumferences() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calculateArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
