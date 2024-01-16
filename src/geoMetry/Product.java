6d367c557fe710c0da574adf9e25f58b72c52cc7
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-01-16 09:41:02 +0100

    created in Product the variables, getter&setters, toString method and 2 methods of the class

--- src/geoMetry/Product.java
+++ src/geoMetry/Product.java
@@ -2,4 +2,27 @@ package geoMetry;
 
 public abstract class Product {
 
+	private double x;
+	private double y;
+	
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	} 
+	
+	public String toString() {
+		return "|" + this.x + "|" + this.y + "|";
+	}
+	
+	public abstract double calculateCircumferences();
+	public abstract double calculateArea();
+	
 }
