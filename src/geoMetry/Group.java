bc2917a9992c0c63ef7679a410008c57ce91627a
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-01-29 11:29:21 +0100

    added Java doc comments

--- src/geoMetry/Group.java
+++ src/geoMetry/Group.java
@@ -7,14 +7,26 @@ public class Group {
 
 	ArrayList<Product> product = new ArrayList<Product>();
 	
+	/**
+	 * Adds a body to the list of all products
+	 * @param product
+	 */
 	public void addProduct(Product product) {
 		this.product.add(product);
 	}
 	
+	/**
+	 * Removes a body from the list of all products 
+	 * @param product
+	 */
 	public void removeProduct(Product product) {
 		this.product.remove(product);
 	}
 	
+	/**
+	 * calculates the whole area of all products
+	 * @return
+	 */
 	public double overallArea() {
 		double area = 0;
 		for(Product p : this.product) {
@@ -23,6 +35,10 @@ public class Group {
 		return area;
 	}
 	
+	/**
+	 * calculates the whole circumference of all products 
+	 * @return
+	 */
 	public double overallCircumference() {
 		double circumference = 0;
 		for(Product p : this.product) {
@@ -31,20 +47,29 @@ public class Group {
 		return circumference;
 	}
 	
+	/**
+	 * prints out all products from the list on the console
+	 */
 	public void printProduct(){
 		for(Product p : this.product) {
 			System.out.println(p);
 		}
 	}
 	
-	public double highestArea() {
-		double max = 0;
-		for (Product p : this.product) {
-		if(p.calculateArea()>max) {
-			max = p.calculateArea();
-			System.out.println("Biggest Body: " + p.toString());
-			}
-		}
-		return max;
+	/**
+	 * prints out the product with the biggest area
+	 * @return the area of this body
+	 */
+	public Product biggestBody() {
+	    Product maxBody = this.product.get(0);
+	    for(Product p : this.product) {
+	        if(p.calculateArea() > maxBody.calculateArea()) {
+	            maxBody = p;
+	        }
+	    }
+	    System.out.println("Biggest Body:\n\t" + maxBody.toString());
+	    return maxBody;
 	}
+
 }
+
