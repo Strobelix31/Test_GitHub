251e073a730c6a5c5c11870effcb5289a7781351
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-01-30 08:41:45 +0100

    added the headline in the methodhead of the class PhotoEntry

--- src/socialNetwork/PhotoEntry.java
+++ src/socialNetwork/PhotoEntry.java
@@ -3,10 +3,12 @@ package socialNetwork;
 public class PhotoEntry extends Entry {
 
     private String filename;
+    private String headline;
 
-    public PhotoEntry(String author, long timestamp, int likes, String filename) {
+    public PhotoEntry(String author, long timestamp, int likes, String filename, String headline) {
         super(author, timestamp, likes);
         this.filename = filename;
+        this.headline = headline;
     }
 
     public String getFilename() {
@@ -17,8 +19,16 @@ public class PhotoEntry extends Entry {
         this.filename = filename;
     }
 
-    @Override
+    public String getHeadline() {
+		return headline;
+	}
+
+	public void setHeadline(String headline) {
+		this.headline = headline;
+	}
+
+	@Override
     public String toString() {
-        return this.getAuthor() + " " + this.getTimestamp() + " " + this.getLikes()+ " " + this.getFilename();
+        return this.getAuthor() + " " + this.getTimestamp() + " " + this.getLikes()+ " " + this.getFilename() +  " " + this.getHeadline();
     }
 }
