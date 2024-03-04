7d828f8a4fe489d88c3e80240a2beed2b1cacc1e
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-03-04 11:45:14 +0100

    created new class and started with a new method

---
+++ src/Arrays/losses.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class losses {
+	
+
+	public static void trimNonGrowingNumbers(List<Double> numbers) {
+		  int lastIndex = 0; 							// Index der letzten Zahl, die noch in der Liste bleiben soll
+		  for (int i = 1; i < numbers.size(); i++) { 	// Schleife über alle Zahlen in der Liste
+			  	if (numbers.get(i) < numbers.get(lastIndex)) {
+			  		
+			  	}
+		  }
+	} 
+		   
+	public static void main(String[] args) {
+	   
+	
+	}		 
+}
