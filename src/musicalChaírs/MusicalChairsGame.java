4c70116d28914bc3f0aa90c729fd50973b16e5bd
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-03-05 09:35:24 +0100

    created in test class some testings for the MusicalChair class

--- src/musicalChaírs/MusicalChairsGame.java
+++ src/musicalChaírs/MusicalChairsGame.java
@@ -3,8 +3,15 @@ package musicalChaírs;
 public class MusicalChairsGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		MusicalChair mc1 = new MusicalChair("lisa", "Susi", "Tom", "Lukas");
+		System.out.println(mc1);
+		
+		MusicalChair mc2 = new MusicalChair ("Madeleine", "Benedikt");
+		System.out.println(mc2);
+		
+		MusicalChair mc3 = new MusicalChair();
+		System.out.println(mc3);
 	}
 
 }
