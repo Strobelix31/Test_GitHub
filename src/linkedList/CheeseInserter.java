ef7c74f215ce04297cae9a08135584e2e2d8d219
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-03-12 08:41:50 +0100

    finished class cheesInserter and finally tested it

--- src/linkedList/CheeseInserter.java
+++ src/linkedList/CheeseInserter.java
@@ -1,13 +1,20 @@
 package linkedList;
 
+// import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.regex.Pattern;
 
 public class CheeseInserter {
+	
+	// private - Sichtbarkeitsbereich auf die Klasse beschränkt
+	// static - von der Klasse abhängig und nicht von Objekt
+	// final - Variable nach Initialisierung unveränderbar
 
 	private static final Pattern vegetables = Pattern.compile(
-			"Zucchini|Paprikas?|Zwiebeln?|Tomaten?"
+			"Zucchini|Paprikas?|Zwiebeln?|Tomaten?|Melanzani"
 			);
 	
 	public static void insertCheeseAroundVegetable(List<String> ingredients) {
@@ -15,13 +22,19 @@ public class CheeseInserter {
 		// Abbruchbedingung (hasNext())
 		// keine Inkrementierung --> passiert im Schleifenkörper
 		for(ListIterator<String> iterator = ingredients.listIterator();iterator.hasNext();) {
-			String ingredient = iterator.next();	
+			String ingredient = iterator.next();
+			// matcher --> wird verwendet, um ingredient in unserem Muster zu finden
+			// matches() --> für den direkten Vergleich
+			if(vegetables.matcher(ingredient).matches()) {
+				iterator.add("Käse");
+			}
 		}
 	}
 	
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) {	
+		List<String> ingredients = new LinkedList<String>();
+		Collections.addAll(ingredients, "Gnocchi", "Paprika", "Tomate", "Pfeffer", "Melanzani");
+		insertCheeseAroundVegetable(ingredients);
+		System.out.println(ingredients);
 	}
-
 }
