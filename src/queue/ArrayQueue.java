5858651b9222dba37f69ea01ab0372ded5cc081f
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-03-12 09:45:25 +0100

    new class in our swp lesson

---
+++ src/queue/ArrayQueue.java
@@ -0,0 +1,40 @@
+package queue;
+
+public class ArrayQueue {
+
+	private static int front;
+	private static int rear;
+	private static int capacity;
+	private static int[] queue;
+	
+	public ArrayQueue(int size) {
+		front = 0;
+		rear = 0;
+		capacity = size;
+		queue = new int[size];
+	}
+	
+	public static void queueEnqueue(int item) {
+		if(rear == capacity) {
+		System.out.println("Queue ist volL!!");
+		}else {
+			queue[rear]=item;
+			rear++;
+		}
+	}
+	
+	public static int queueDequeue() {
+		if(rear == front) {
+			System.out.println("Queue isr leer");
+		}else {
+			int firstElement = queue[front];
+			}	
+		}
+}
+	
+	
+	
+	
+	
+	
+}
